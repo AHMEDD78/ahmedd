f0efc33f2c368169e6d52138ba6cd9619ab9aff0
AHMEDD78 <72225264+AHMEDD78@users.noreply.github.com>
2020-10-01 20:04:38 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  tokens: [
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+    "token",
+  ], ///توكنات حسابات الترحيب
+  voice_channel: "759234356467466251", ///ايدي الروم اللي بدك يثبت فيه
+  text_channel: "750741284347445258", ///ايدي روم اللي بدك يرحب فيه
+  your_server: "721019622240944129" /// ايدي السيرفر تبعك
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+const {
+  tokens,
+  voice_channel,
+  text_channel,
+  your_server
+} = require("./config.js");
+const time = [];
+const Discord = require("discord.js");
+tokens.forEach(token => {
+  const client = new Discord.Client();
+  client.login(token);
+
+  client.on("message", async message => {
+    if (message.guild.id != your_server) return;
+    if (
+      message.author.id == "282859044593598464" && // تعديل مهم ايدي بوت الترحيب
+      message.content.includes("SALAM") /// (الاستقبال) تعديل مهم رسالة الترحيب
+    ) {
+      setTimeout(() => {
+        message.channel.send("SALAM"); ///  (الارسال)تعديل مهم رسالة الترحيب
+      }, time[Math.floor(Math.random() * time.length)]);
+    }
+  });
+
+  client.on("ready", () => {
+    console.log(client.user.tag + " is ready!");
+    client.guilds
+      .get(your_server)
+      .channels.get(voice_channel)
+      .join();
+  });
+});
